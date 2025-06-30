bb5a6968d4aa8b54d4fe105be3c1089fe599bde4
Hesham Mohamed Ahamed <94226077+abo-habeba@users.noreply.github.com>
2025-06-30 13:20:36 +0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\Blameable;
+use App\Traits\Scopes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Product extends Model
 {
-    use HasFactory, Blameable;
+    use HasFactory, Blameable,Scopes;
 
     protected $fillable = [
         'name',
