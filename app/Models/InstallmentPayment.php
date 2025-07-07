373016cb59740e3619111e87f3b6b5e3c9646942
Hesham Mohamed Ahamed <94226077+abo-habeba@users.noreply.github.com>
2025-07-08 02:01:59 +0300

    Update InstallmentPayment.php

--- app/Models/InstallmentPayment.php
+++ app/Models/InstallmentPayment.php
@@ -13,15 +13,41 @@ use Illuminate\Database\Eloquent\Model;
 class InstallmentPayment extends Model
 {
     use HasFactory, Scopes, Blameable;
+
     protected $fillable = [
         'installment_plan_id',
+        'company_id',
+        'created_by',
         'payment_date',
         'amount_paid',
         'payment_method',
-        'notes'
+        'notes',
     ];
+
     public function plan()
     {
         return $this->belongsTo(InstallmentPlan::class, 'installment_plan_id');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function details()
+    {
+        return $this->hasMany(InstallmentPaymentDetail::class, 'installment_payment_id');
+    }
+
+    public function installments()
+    {
+        return $this->belongsToMany(Installment::class, 'installment_payment_details')
+            ->withPivot('amount_paid')
+            ->withTimestamps();
     }
 }
