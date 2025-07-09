e7fb0387aec21aedbe2e16d83c6d8c0a454339ba
heshame <atsh@gmail.com>
2025-07-09 11:56:05 +0300

    total_installments_remaining total_installments_amount total_installments_pay

--- app/Http/Resources/Installment/InstallmentResource.php
+++ app/Http/Resources/Installment/InstallmentResource.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace App\Http\Resources\Installment;
 
 use App\Http\Resources\InstallmentPlan\InstallmentPlanBasicResource;
 use App\Http\Resources\User\UserBasicResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+
 class InstallmentResource extends JsonResource
 {
     public function toArray($request)
@@ -19,8 +21,8 @@ class InstallmentResource extends JsonResource
             'remaining' => $this->remaining,
             'created_by' => $this->created_by,
             'user_id' => $this->user_id,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at ? $this->created_at->toDateTimeString() : null,
+            'updated_at' => $this->updated_at ? $this->updated_at->toDateTimeString() : null,
             'user' => new UserBasicResource($this->whenLoaded('user')),
             'creator' => new UserBasicResource($this->whenLoaded('creator')),
             'installment_plan' => new InstallmentPlanBasicResource($this->whenLoaded('installmentPlan')),
