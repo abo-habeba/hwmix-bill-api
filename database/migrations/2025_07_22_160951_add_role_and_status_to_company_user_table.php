d200b648d971528fd71f254d01ea48eea8d6f4e6
heshame <atsh@gmail.com>
2025-07-23 19:30:00 +0300

    Add role and status columns to company_user table

--- database/migrations/2025_07_22_160951_add_role_and_status_to_company_user_table.php
+++ database/migrations/2025_07_22_160951_add_role_and_status_to_company_user_table.php
@@ -4,22 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('company_user', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
-
+        Schema::table('company_user', function (Blueprint $table) {
             $table->enum('role', ['manager', 'employee', 'customer'])->default('customer')->nullable();
             $table->enum('status', ['active', 'pending', 'suspended'])->default('active')->nullable();
-
-            $table->timestamps();
         });
     }
 
@@ -28,6 +22,9 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('company_user');
+        Schema::table('company_user', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('status');
+        });
     }
 };
